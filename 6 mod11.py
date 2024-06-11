9a249041b4e972adcd6f42a67b977cf0aa5895ac
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2024-06-11 08:14:33 +0530

    Add files via upload

---
+++ 6 mod11.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This
+ is a temporary script file.
+"""
+
+def show():                        # function definition
+    print('Hello World')
+    print('Welcome')
+    print('Goodbye!!!')
